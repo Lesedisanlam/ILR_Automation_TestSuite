88803f1ad305c7581f8917ba12af6b96664f7b32
NapeB <napeb@sanlamsky.co.za>
2022-03-31 20:09:23 +0200

    updates

--- src/main/java/Testbase/Tbase.java
+++ src/main/java/Testbase/Tbase.java
@@ -1,16 +1,37 @@
 package Testbase;
 
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-public class Tbase {
 
+public class Tbase {
+	ExtentReports extent;
+@BeforeTest
+
+public void config(){
+	// extentreports ,extentsparkreportter
+
+String path =	System.getProperty("user.dir")+"\\index.html";
+	ExtentSparkReporter report = new ExtentSparkReporter(path);
+     report.config().setReportName("ILR Policy servicing Automation Results");
+     report.config().setDocumentTitle("Test Results");
+
+	 extent = new ExtentReports();
+extent.attachReporter(report);
+ extent.setSystemInfo("Tester","Nape Boshielo");
+
+
+}
 
 	@Test
 	 public void policy() {
 
+	extent.createTest("ILR Demo");
 	        System.setProperty("webdriver.chrome.driver","C:\\Code\\bin\\ChromeDriver.exe");
 	       WebDriver _driver = new ChromeDriver();
 	       _driver.get("http://ilr-int.safrican.co.za/web/wspd_cgi.sh/WService=wsb_ilrint/run.w?");
@@ -20,8 +41,7 @@ public class Tbase {
 	        _driver.findElement(By.name("fcPassword")).sendKeys("Aw123456");
 
 	        _driver.findElement(By.name("btnLogin")).click();
-
-
+         extent.flush();
 	    }
 
 	
